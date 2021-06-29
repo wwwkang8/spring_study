a28c688e9fe3df1806f71277f9fb31d5c096c2a9
wwwkang8 <wwwkang8@gmail.com>
2021-06-29 22:52:39 +0900

    Java JSON WEB TOKEN

---
+++ src/main/java/com/kosta/jwt/SecurityService.java
@@ -0,0 +1,12 @@
+package com.kosta.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SecurityService {
+
+    String createToken(String subject, long ttlMillis);
+
+    String getSubject(String token)
+
+}
