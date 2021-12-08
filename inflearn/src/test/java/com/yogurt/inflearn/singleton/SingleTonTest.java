cb47177dc300c3f31dea76e5daa92391bb6d70b7
wwwkang8 <wwwkang8@gmail.com>
2021-12-08 20:24:58 +0900

    싱글톤 컨테이너

--- inflearn/src/test/java/com/yogurt/inflearn/singleton/SingleTonTest.java
+++ inflearn/src/test/java/com/yogurt/inflearn/singleton/SingleTonTest.java
@@ -5,6 +5,9 @@ import com.yogurt.inflearn.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingleTonTest {
 
@@ -21,6 +24,23 @@ public class SingleTonTest {
         System.out.println("memberService2 = "+memberService2);
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //싱글톤이기 때문에 동일하다
+        System.out.println("memberService1 = "+memberService1);
+        System.out.println("memberService2 = "+memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 
 }
